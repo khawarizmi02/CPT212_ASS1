2693a9643e6145eeef9627785f9c8c643311c035
khawarizmi <khawarizmijefri02@gmail.com>
2023-05-15 11:30:12 +0800

    delete main.java and main.class, update on float.java

--- src/floating.java
+++ src/floating.java
@@ -1,142 +1,107 @@
-//
-//   // Radix Sort in Java Programming
-//
-// import java.util.Arrays;
-//
-// class floating {
-//
-//     // Using counting sort to sort the elements in the basis of significant places
-//     void countingSort(float array[], int size, float place) {
-//         float[] output = new float[size + 1];
-//         float max = array[0];
-//         for (int i = 1; i < size; i++) {
-//             if (array[i] > max)
-//                 max = array[i];
-//         }
-//         int[] count = new int[max + 1];
-//
-//         for (int i = 0; i < max; ++i)
-//             count[i] = 0;
-//
-//         // Calculate count of elements
-//         for (int i = 0; i < size; i++)
-//             count[(array[i] / place) % 10]++;
-//
-//         // Calculate cumulative count
-//         for (int i = 1; i < 10; i++)
-//             count[i] += count[i - 1];
-//
-//         // Place the elements in sorted order
-//         for (int i = size - 1; i >= 0; i--) {
-//             output[count[(array[i] / place) % 10] - 1] = array[i];
-//             count[(array[i] / place) % 10]--;
-//         }
-//
-//         for (int i = 0; i < size; i++)
-//             array[i] = output[i];
-//     }
-//
-//     // Function to get the largest element from an array
-//     int getMax(float array[], int n) {
-//         float max = array[0];
-//         for (int i = 1; i < n; i++)
-//             if (array[i] > max)
-//                 max = array[i];
-//         return max;
-//     }
-//
-//     // Main function to implement radix sort
-//     void radixSort(float array[], int size) throws InterruptedException {
-//         // Get maximum element
-//         float max = getMax(array, size);
-//
-//         System.out.println("Max value: ");
-//         System.out.println(max);
-//         int index = 1;
-//         // Apply counting sort to sort elements based on place value.
-//         for (float place = 1.0; max / place > 0; place *= 10) {
-//             countingSort(array, size, place);
-//
-//             System.out.println("sorted Array Order for index of " + index + ":" );
-//             System.out.println(Arrays.toString(array));
-//             Thread.sleep(2000);
-//             index++;
-//         }
-//     }
-//
-//     // Driver code
-//     public static void main(String args[]) throws InterruptedException {
-//
-//         // float[] data = { 275, 87, 426, 061, 409, 170, 677, 503 };
-//         float[] data = { 1.2, 1.3, 0.12, 2.12343, 2.543, 0.0012, 0.12, 1.25 };
-// //        int[] data = { 121, 432, 564, 23, 1, 45, 788 };
-//         int size = data.length;
-//         System.out.println("current Array Order: ");
-//
-//         System.out.println(Arrays.toString(data));
-//         Thread.sleep(2000);
-//         floating rs = new floating();
-//         rs.radixSort(data, size);
-//         System.out.println("Sorted Array in Ascending Order: ");
-//         System.out.println(Arrays.toString(data));
-//     }
-// }
+// modified code from Radix.java
 
-import java.lang.System;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
- class floating {
+class floating {
 
-     void countingSort(double exp, double array[])
-    {
-        int[] count = new int[10];
-        double[] output = new double[array.length];
+	// The main function to that sorts arr[] of
+	// size n using Radix Sort
+	static void radixsort(float arr[], int n)
+	{
+    // const number to time all element with it
+    float num = (float) Math.pow(10,7);
 
-        for (double value : array) {
-            count[(int) ((value / exp) % 10)]++;
-        }
+		// Find the maximum number to know number of digits
+		float m = getMax(arr, n);
 
-        for (int i = 1; i < 10; i++) {
-            count[i] += count[i - 1];
-        }
+    // Change all float value inside of the array to whole number
+    for (int i = 0; i < n; i++)
+      arr[i] = arr[i] * num;
+    // System.out.println(Arrays.toString(arr));
 
-        for (int i = array.length - 1; i >= 0; i--) {
-            output[count[(int) ((array[i] / exp) % 10)] - 1] = array[i];
-            count[(int) ((array[i] / exp) % 10)]--;
-        }
 
-        System.arraycopy(output, 0, array, 0, array.length);
-    }
+    // Max value will change to int number
+    int c = (int) m * (int) num;
 
-    // Function to get the largest element from an array
-    double getMax(double array[], int n) {
-        double max = array[0];
-        for (int i = 1; i < n; i++)
-            if (array[i] > max)
-                max = array[i];
-        return max;
-    }
+		// Do counting sort for every digit. Note that
+		// instead of passing digit number, exp is passed.
+		// exp is 10^i where i is current digit number
+		for (int exp = 1; c / exp > 0; exp *= 10)
+			countSort(arr, n, exp);
 
-    void radixSort(double array[], int size) {
-        double max = getMax(array , size);
-        for (double exp = 1; max / exp > 0; exp *= 10) {
-            countingSort(exp, array);
-        }
-    }
+    // change back all sorted number into float number
+    for (int i = 0; i < n; i++)
+      arr[i] = arr[i] / num;
+	}
 
-     public static void main(String[] args) {
-       double data[] = { 170, 45.2, 75.69, 90.68, 802, 24, 2, 66 };
-       int size = data.length;
+	// A function to do counting sort of arr[] according to
+	// the digit represented by exp.
+	static void countSort(float arr[], int n, int exp)
+	{
+    // System.out.println("We are in the countSort function: \n");
+		float output[] = new float[n]; // output array
+		int i;
+		int count[] = new int[10];
+    // store value 0 to all indexes of count array
+		Arrays.fill(count, 0);
+    // System.out.println(Arrays.toString(count));
 
-       System.out.println("Unsorted arrray list: ");
-       System.out.println(Arrays.toString(data));
+		// Store count of occurrences in count[]
+		for (i = 0; i < n; i++)
+			count[((int)arr[i] / exp) % 10]++;
+    // System.out.println(Arrays.toString(count));
 
-       floating rs = new floating();
+		// Change count[i] so that count[i] now contains
+		// actual position of this digit in output[]
+		for (i = 1; i < 10; i++)
+			count[i] += count[i - 1];
+    // System.out.println(Arrays.toString(count));
 
-       rs.radixSort(data, size);
- 
-       System.out.println("Sorted arrray list: ");
-       System.out.println(Arrays.toString(data));
-    }
+		// Build the output array
+		for (i = n - 1; i >= 0; i--) {
+			output[count[((int)arr[i] / exp) % 10] - 1] = arr[i];
+			count[((int)arr[i] / exp) % 10]--;
+		}
+    // System.out.println(Arrays.toString(count));
+    // System.out.println(Arrays.toString(output));
+
+		// Copy the output array to arr[], so that arr[] now
+		// contains sorted numbers according to current digit
+		for (i = 0; i < n; i++)
+			arr[i] = output[i];
+	}
+    // System.out.println(Arrays.toString(arr));
+
+	// A utility function to print an array
+	static void print(float arr[], int n)
+	{
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+      System.out.println(" \n");
+	}
+
+	// A utility function to get maximum value in arr[]
+	static float getMax(float arr[], int n)
+	{
+		float mx = arr[0];
+		for (int i = 1; i < n; i++)
+			if (arr[i] > mx)
+				mx = arr[i];
+		return mx;
+	}
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+    float[] arr = {9.1f, 3.387921f, 123.198456f, 12.945678f, 0.521345f, 0.876543f, 56.234567f, 0.612345f, 0.098765f, 0.456789f};
+		int n = arr.length;
+		print(arr, n);
+
+		// Function Call
+		radixsort(arr, n);
+		print(arr, n);
+	}
 }
+
 
