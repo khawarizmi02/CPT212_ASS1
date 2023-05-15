bda48566ff45094e77b11f80f2189c0f500f82f1
khawarizmi <khawarizmijefri02@gmail.com>
2023-05-15 13:13:20 +0800

    The program will randomly create number and the array size will increse from 1 to 15, increament by 5. Trying to add operator counter

--- src/Radix.java
+++ src/Radix.java
@@ -2,31 +2,42 @@
 
 import java.io.*;
 import java.util.*;
+import java.util.Random;
 
 class Radix {
 
-	// A utility function to get maximum value in arr[]
-	static int getMax(int arr[], int n)
+	// The main function to that sorts arr[] of
+	// size n using Radix Sort
+	static void radixsort(int arr[], int n, int count_operator)
 	{
-		int mx = arr[0];
-		for (int i = 1; i < n; i++)
-			if (arr[i] > mx)
-				mx = arr[i];
-		return mx;
+		// Find the maximum number to know number of digits
+		int m = getMax(arr, n);
+
+		// Do counting sort for every digit. Note that
+		// instead of passing digit number, exp is passed.
+		// exp is 10^i where i is current digit number
+		for (int exp = 1; m / exp > 0; exp *= 10)
+			countSort(arr, n, exp, count_operator);
+
+    // Print out the result of the operator counter
+    System.out.println("Operator counter: " + count_operator + " \n");
 	}
 
+ 
 	// A function to do counting sort of arr[] according to
 	// the digit represented by exp.
-	static void countSort(int arr[], int n, int exp)
+	static void countSort(int arr[], int n, int exp, int count_operator)
 	{
 		int output[] = new int[n]; // output array
 		int i;
 		int count[] = new int[10];
 		Arrays.fill(count, 0);
+    count_operator += 4;
 
 		// Store count of occurrences in count[]
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n; i++){
 			count[(arr[i] / exp) % 10]++;
+    }
 
 		// Change count[i] so that count[i] now contains
 		// actual position of this digit in output[]
@@ -46,18 +57,14 @@ class Radix {
 			arr[i] = output[i];
 	}
 
-	// The main function to that sorts arr[] of
-	// size n using Radix Sort
-	static void radixsort(int arr[], int n)
+	// A utility function to get maximum value in arr[]
+	static int getMax(int arr[], int n)
 	{
-		// Find the maximum number to know number of digits
-		int m = getMax(arr, n);
-
-		// Do counting sort for every digit. Note that
-		// instead of passing digit number, exp is passed.
-		// exp is 10^i where i is current digit number
-		for (int exp = 1; m / exp > 0; exp *= 10)
-			countSort(arr, n, exp);
+		int mx = arr[0];
+		for (int i = 1; i < n; i++)
+			if (arr[i] > mx)
+				mx = arr[i];
+		return mx;
 	}
 
 	// A utility function to print an array
@@ -71,13 +78,37 @@ class Radix {
 	// Main driver method
 	public static void main(String[] args)
 	{
-		int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
-		int n = arr.length;
-		print(arr, n);
+    
+    int n;
+    int count_operator = 0; // Operator counter
+    int [] array; // Define an array
+    Random rand = new Random(); // Define random function
 
-		// Function Call
-		radixsort(arr, n);
-		print(arr, n);
+    array = new int[1];
+    array[0] = rand.nextInt(99999) + 1;
+    System.out.println(array[0]);
+    n = array.length;
+    radixsort(array, n, count_operator);
+    print(array, n);
+    
+    for (int i = 5; i <= 15; i = i + 5){
+        array = new int[i];
+        System.out.println("Create an array with the size of " + i + " : \n");
+        for (int k = 0; k < array.length; k++) {
+            array[k] = rand.nextInt(99999) + 1; // Input a random number between 1 to 99999
+        }
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " "); // Print all list of random number
+        }
+        System.out.println(" \n");
+
+        // Function call to the radixSort
+        n = array.length; // Get the length of the array
+        count_operator = 0; // Set the operator counter to 0
+        radixsort(array, n, count_operator); 
+        System.out.println("Result of the radix sort: \n");
+        print(array, n);
+    }
 	}
 }
 
